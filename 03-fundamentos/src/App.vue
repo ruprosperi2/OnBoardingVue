487b478caecb4310a320ca11125fb4c4f9ffb5c7
ruprosperi2 <rubenjariasp@gmail.com>
2021-09-28 19:26:57 -0400

    Video 60: Watch - observar cambios en una propiedad reactiva

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <indecision/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Indecision from "./components/Indecision";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   indecision: Indecision
   }
 }
 </script>
---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,59 @@
+<template>
+  <h2>{{customTitle}}:</h2>
+  <p>{{ counter }} <sup>2</sup>={{ power }}</p>
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Patito",
+  props:{
+    title: String,
+    start:{
+      type: Number
+    }
+  },
+  data() {
+    return {
+      counter: 5
+    }
+  },
+  computed: {
+    power() {
+      return this.counter * this.counter
+    },
+    customTitle(){
+      return this.title ? this.title :'Counter'
+    }
+  }, methods:{
+    increase(){
+      this.counter++
+    },
+    decrease(){
+      this.counter--
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  background-color: #64b6b7;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+
+button:hover{
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
+
+</style>
